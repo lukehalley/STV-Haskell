17e4a9b26f737d7e9c376cd328917bd35c1d1cfd
lukehalley <luke123halley@gmail.com>
2019-03-17 12:40:19 +0000

    init commit with CSV and simple list

---
+++ vote.hs
@@ -0,0 +1,9 @@
+votes = [
+            ["Rachet","Clank","Sly Racoon","Jak","Daxter"],
+            ["Sly Racoon","Rachet","Clank","Daxter","Jak"],
+            ["Clank","Rachet","Sly Racoon","Jak","Daxter"],
+            ["Rachet","Clank","Sly Racoon","Jak","Daxter"],
+            ["Clank","Rachet","Sly Racoon","Jak","Daxter"],
+            ["Sly Racoon","Rachet","Clank","Daxter","Jak"]
+        ]
+
